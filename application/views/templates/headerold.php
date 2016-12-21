c2e968fec82237aad6667fda3d699918decc2859
NightSnack <chiyexiao@icloud.com>
2016-12-21 13:07:51 +0800

    add some untracked files

---
+++ application/views/templates/headerold.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet"
+	href="<?php echo base_url('assets/css/bootstrap/css/bootstrap.min.css'); ?>">
+<link href="<?php echo base_url('assets/css/font-awesome/css/font-awesome.min.css'); ?>"
+	rel="stylesheet">
+<link rel="stylesheet"
+	href="<?php echo base_url('assets/css/AdminLTE/css/AdminLTE.css'); ?>">
+<link rel="stylesheet"
+	href="<?php echo base_url('assets/css/AdminLTE/css/skins/_all-skins.min.css'); ?>">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+        <!-- jQuery 2.1.4 -->
+    <script src="<?php echo base_url('assets/js/jQuery-2.1.4.min.js'); ?>"></script>
+     
+  
+  </head>
+  <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+  <body class="hold-transition skin-red fixed layout-top-nav">
+    <div class="wrapper">
+
+           <header class="main-header">
+        <nav class="navbar navbar-static-top">
+          <div class="container">
+            <div class="navbar-header">
+              <a href="<?php echo base_url()?>" class="navbar-brand"><b>i</b>Service</a>
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+                <i class="fa fa-bars"></i>
+              </button>
+            </div>
+
+            <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+              <ul class="nav navbar-nav">
+                <li class="dropdown">
+                  <a  href="#" class="dropdown-toggle" data-toggle="dropdown">失物招领 <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="<?php echo site_url("Home/FindSquare") ?>">招领启事</a></li>
+                    <li><a href="<?php echo site_url("Home/LoseSquare") ?>">寻物启事</a></li>
+                    <li><a href="<?php echo site_url("Home/InsertFind") ?>">发布招领启事</a></li>
+                    <li><a href="<?php echo site_url("Home/InsertLose") ?>">发布寻物启事</a></li>
+                    <li><a href="<?php echo site_url("Person/myFind") ?>">我的招领启事</a></li>
+                    <li><a href="<?php echo site_url("Person/myLose") ?>">我的寻物启事</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a  href="#" class="dropdown-toggle" data-toggle="dropdown">通知公告 <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="http://wechat.aifuwu.org/notification/notification/notification_list/njupt">校级通知</a></li>
+                    <li><a href="http://wechat.aifuwu.org/notification/notification/notification_list/jwc">教务处</a></li>
+                    <li><a href="http://wechat.aifuwu.org/notification/notification/notification_list/xsc">学工处</a></li>
+                    <li><a href="http://wechat.aifuwu.org/notification/notification/notification_list/bwc">保卫处</a></li>
+                    <li><a href="http://wechat.aifuwu.org/notification/notification/notification_list/hqc">后勤处</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a  href="#" class="dropdown-toggle" data-toggle="dropdown">招聘信息 <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="http://wechat.aifuwu.org/recruit/recruit/notification_list/job_campus">招聘公告</a></li>
+                    <li><a href="http://wechat.aifuwu.org/recruit/recruit/notification_list/teachin">招聘宣讲会</a></li>
+                  </ul>
+                </li>
+
+                <li><a href="http://wechat.aifuwu.org/confession_wall/">表白墙</a></li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+      </header>
---
+++ readme.md
@@ -0,0 +1,7 @@
+##Lost and Found
+
+#####on wechat:njuptservice
+###Notice
+
+1.import SQL.zip
+2.delete application/config/database.php & memcached.php
